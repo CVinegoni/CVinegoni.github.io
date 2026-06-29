3bec1c924007ca078745be206e5311bf6a20147f
Claudio Vinegoni <vinegoni@gmail.com>
2026-06-29 13:01:08 -0400

    added icon isbn

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,19 +1,22 @@
 $(document).ready(function () {
-    $('a.abstract').click(function () {
-        $(this).parent().parent().find(".abstract.hidden").toggleClass('open');
-    });
-    $('a.bibtex').click(function () {
-        $(this).parent().parent().find(".bibtex.hidden").toggleClass('open');
-    });
-    $('a.pmid').click(function () {
-        $(this).parent().parent().find(".pmid.hidden").toggleClass('open');
-    });
+  $("a.abstract").click(function () {
+    $(this).parent().parent().find(".abstract.hidden").toggleClass("open");
+  });
+  $("a.bibtex").click(function () {
+    $(this).parent().parent().find(".bibtex.hidden").toggleClass("open");
+  });
+  $("a.pmid").click(function () {
+    $(this).parent().parent().find(".pmid.hidden").toggleClass("open");
+  });
 
-    $('a.doi').click(function () {
-        $(this).parent().parent().find(".doi.hidden").toggleClass('open');
-    });
-    $('a.pmcid').click(function () {
-        $(this).parent().parent().find(".pmcid.hidden").toggleClass('open');
-    });
-    $('.navbar-nav').find('a').removeClass('waves-effect waves-light');
+  $("a.doi").click(function () {
+    $(this).parent().parent().find(".doi.hidden").toggleClass("open");
+  });
+  $("a.pmcid").click(function () {
+    $(this).parent().parent().find(".pmcid.hidden").toggleClass("open");
+  });
+  $(".navbar-nav").find("a").removeClass("waves-effect waves-light");
+});
+$("a.isbn").click(function () {
+  $(this).parent().parent().find(".isbn.hidden").toggleClass("open");
 });
